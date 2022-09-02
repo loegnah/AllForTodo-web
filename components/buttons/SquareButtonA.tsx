5c71f6ea5f1a7bd09e228cd03a945eccce96d389
loegnah <loegnah@gmail.com>
2022-09-02 23:30:22 +0900

    Emotion setting

---
+++ components/buttons/SquareButtonA.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+function SquareButtonA() {
+  return <Button>Hi</Button>;
+}
+
+const Button = styled.button`
+  color: turquoise;
+`;
+
+export default SquareButtonA;
