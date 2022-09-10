bea3efcfe8a78f9e695508bedd994924d30c66bd
loegnah <loegnah@gmail.com>
2022-09-10 22:25:30 +0900

    Make navigation bar icon

--- components/navBars/RootNavBar.tsx
+++ components/navBars/RootNavBar.tsx
@@ -1,12 +1,33 @@
 import styled from '@emotion/styled';
+import SquareIconButton from '/components/buttons/SquareIconButton';
+
+// FIXME : call api for getting user selected buttons
+const userSelectedButtonNames = ['calendar', 'calendar'];
 
 function RootNavBar() {
-  return <Bar></Bar>;
+  return (
+    <Bar>
+      {userSelectedButtonNames.map((name, i) => (
+        <IconContainer key={i}>
+          <SquareIconButton />
+        </IconContainer>
+      ))}
+    </Bar>
+  );
 }
 
 const Bar = styled.section`
   background-color: rosybrown;
-  width: 50px;
+  width: 60px;
+  padding-top: 5px;
+  align-items: center;
+  gap: 5px;
+`;
+
+const IconContainer = styled.div`
+  width: 100%;
+  align-items: center;
+  padding: 5px 0;
 `;
 
 export default RootNavBar;
