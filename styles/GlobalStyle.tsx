bd738f95603dae8e97c58f65c52556404493c9b6
loegnah <loegnah@gmail.com>
2022-09-23 22:10:15 +0900

    twin.macro initial set

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -1,47 +1,33 @@
-import { Global, css, useTheme } from '@emotion/react';
+import { Global, Theme, useTheme } from '@emotion/react';
+import { css, GlobalStyles as TwinGlobalStyles } from 'twin.macro';
 
 function GlobalStyle() {
   const theme = useTheme();
   return (
-    <Global
-      styles={css`
-        * {
-          box-sizing: border-box;
-        }
-
-        html,
-        body {
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans,
-            Droid Sans, Helvetica Neue, sans-serif;
-          color: ${theme.color.font};
-        }
-
-        div,
-        main,
-        section,
-        article {
-          display: flex;
-          flex-direction: column;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        //@media (prefers-color-scheme: dark) {
-        //  html {
-        //    color-scheme: dark;
-        //  }
-        //  body {
-        //    color: white;
-        //    background: black;
-        //  }
-        //}
-      `}
-    />
+    <>
+      <TwinGlobalStyles />
+      <Global styles={globalStyles(theme)} />
+    </>
   );
 }
+
+const globalStyles = (theme: Theme) => [
+  css`
+    html,
+    body {
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+        Helvetica Neue, sans-serif;
+      color: ${theme.color.font};
+    }
+
+    div,
+    main,
+    section,
+    article {
+      display: flex;
+      flex-direction: column;
+    }
+  `,
+];
 
 export default GlobalStyle;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        abc: '#1fb6ff',
+      },
+    },
+  },
+  plugins: [],
+};
