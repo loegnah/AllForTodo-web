864e6231b989e242e1b78c7ec1a00f8ac0b7b497
loegnah <loegnah@gmail.com>
2022-09-26 22:40:01 +0900

    Add left-right button

--- components/calendar/CalendarHeader.tsx
+++ components/calendar/CalendarHeader.tsx
@@ -11,9 +11,15 @@ function CalendarHeader({ year, month }: Props) {
     <CalendarHeaderLayout>
       <LeftBox>
         <MonthBox>
-          {`${year}Y ${month}M`}
+          {`${year}년 ${month}월`}
           <Image src="/down.svg" alt="me" width="16" height="16" />
         </MonthBox>
+        <IconBox>
+          <Image src="/left.svg" alt="me" width="20" height="20" />
+        </IconBox>
+        <IconBox>
+          <Image src="/right.svg" alt="me" width="20" height="20" />
+        </IconBox>
       </LeftBox>
       <RightBox></RightBox>
     </CalendarHeaderLayout>
@@ -22,10 +28,15 @@ function CalendarHeader({ year, month }: Props) {
 
 const CalendarHeaderLayout = styled.header(tw`h-16 py-4 px-4 flex flex-row justify-between items-center`);
 
-const LeftBox = styled.div(tw`flex flex-row space-x-4`);
+const LeftBox = styled.div(tw`h-full flex flex-row space-x-4 items-center`);
 const MonthBox = styled.div(
-  tw`flex flex-row pl-2 py-0.5 rounded-md font-semibold ease-out duration-100`,
-  tw`hover:ring-2 hover:ring-slate-300 hover:bg-slate-200`
+  tw`flex flex-row pl-2 py-0.5 rounded-md text-lg font-semibold ease-out duration-100`,
+  tw`hover:ring-2 hover:ring-slate-200 hover:bg-slate-50`
+);
+
+const IconBox = styled.div(
+  tw`relative w-6 h-6 flex justify-center items-center rounded-full ring-1 ring-slate-200 ease-out duration-100`,
+  tw`hover:ring-2 hover:ring-slate-300 hover:bg-slate-50`
 );
 
 const CenterBox = styled.div(tw``);
